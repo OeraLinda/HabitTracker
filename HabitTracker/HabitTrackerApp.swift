31c49655b27444ab35f96a6a9528c697e544e3e3
Yasin <yasinerdemli35@icloud.com>
2022-04-21 11:27:00 +0300

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Yasin Erdemli on 21.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
